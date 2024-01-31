9b3a7c165743da5c0e2f7c9829cf1f0d55a52d46
ChikangaTakudzwa <chikangatakudzwa@outlook.com>
2024-01-31 10:33:20 +0000

    added bun install in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-source /home/gitpod/.bashrc 
+#source /home/gitpod/.bashrc 
 
-Run bun run dev to start the dev server. CTRL+C to stop.
-Add frameworks like react or tailwind using astro add.
+#Run bun run dev to start the dev server. CTRL+C to stop.
+#Add frameworks like react or tailwind using astro add.
+
+run npm install -g bun
